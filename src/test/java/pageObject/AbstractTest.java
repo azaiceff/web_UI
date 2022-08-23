4456933c7bd75d238d08ddce88805130b77f09ac
Александр <azaiceff@mail.ru>
2022-08-23 18:36:02 +0300

    Homework 7

--- src/test/java/pageObject/AbstractTest.java
+++ src/test/java/pageObject/AbstractTest.java
@@ -1,38 +1,55 @@
 package pageObject;
 
+import JUnit5.TriangleTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
+import java.util.List;
 
 public class AbstractTest {
     //private static WebDriver driverGoogleChrome;
+    private static final Logger logger = LoggerFactory.getLogger(TriangleTest.class);
     private static EventFiringWebDriver eventDriver;
 
     @BeforeAll
     static void googleChromeDriver() {
-        WebDriverManager.firefoxdriver().setup();
-        FirefoxOptions optionsFox = new FirefoxOptions();
-        optionsFox.addArguments("--incognito");
-        optionsFox.addArguments("--headless");
-        optionsFox.addArguments("start-maximized");
-        eventDriver = new EventFiringWebDriver(new FirefoxDriver(optionsFox));
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("start-maximized");
+        options.setPageLoadTimeout(Duration.ofSeconds(10));
+
+        eventDriver = new EventFiringWebDriver(new ChromeDriver(options));
         eventDriver.register(new MyWebDriverEventListener());
 
-        eventDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        eventDriver.manage().timeouts().implicitlyWait(Duration.ofMillis(200));
     }
 
     @BeforeEach
     void goTo() {
         Assertions.assertDoesNotThrow(() -> getDriver().navigate().to("https://www.livejournal.com/"),
                 "Страница не доступна");
+    }
+
+    @AfterEach
+    public void checkBrowser() {
+        List<LogEntry> allLogRows = getDriver().manage().logs().get(LogType.BROWSER).getAll();
+        if (!allLogRows.isEmpty()) {
+            allLogRows.forEach(logEntry -> {
+                logger.info(logEntry.getMessage());
+            });
+        }
     }
 
     @AfterAll
